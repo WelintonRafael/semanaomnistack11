2df0e44103d789f6e181284b13bcc7d60346ecaa
Welinton Rafale <welintonrs@gmail.com>
2020-03-26 23:08:49 -0300

    correcao do index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app  = express();
-//https://github.com/WelintonRafael/semanaomnistack11.git
+
 app.use(cors());
 app.use(express.json());
 
